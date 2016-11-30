938e0735443c6e940d908d7fec8e9835196195df
Drew Harwell <fenwick67@users.noreply.github.com>
2016-11-29 18:40:58 -0600

    remove jquery dependency (not tested though)

--- vegeta.js
+++ vegeta.js
@@ -1,34 +1,41 @@
 function Vegeta(){
-
+  
+  var $ = function(sel){
+    if (sel[0] == "#" && sel.indexOf(' ') == -1){// #id => by ID (faster), else query selector
+      return document.getElementById( sel.slice(1) ) || document.createElement("div");
+    }
+    return document.querySelector(sel) || document.createElement("div");
+  };
+  
   this.bind_nav_toggle = function(){
     var $toggle = $('.nav-toggle');
     var $menu = $('.nav-menu');
-    $toggle.click(function() {
-      $(this).toggleClass('is-active');
-      $menu.toggleClass('is-active');
+    $toggle.addEventListener('click',function() {
+      this.classList.toggle('is-active');
+      $menu.classList.toggle('is-active');
     });
   };
-
+  
   this.bind_notif = function(sel){
-    $(sel).html(`
+    $(sel).innerHTML = `
       <div style='display: none;' id='vgt-balloon' class="notification">
         <button class="delete"></button>
         <span>Loading..</span>
       </div>
-    `);
-    $('#vgt-balloon .delete').click(function(){
-      $('#vgt-balloon').hide();
+    `;
+    $('#vgt-balloon .delete').addEventListener('click',function(){
+      $('#vgt-balloon').style.visibility = "hidden";
     });
   };
-
+  
   this.notif = function(msg, type){
-    $('#vgt-balloon').removeClass();
+    $bal = $('#vgt-balloon');
     if(type){
-      $('#vgt-balloon').addClass('is-'+type);
+      $('#vgt-balloon').classList.add('is-'+type);
     }
-    $('#vgt-balloon').addClass('notification');
-    $('#vgt-balloon span').html(msg);
-    $('#vgt-balloon').show();
+    $('#vgt-balloon').classList.add('notification');
+    $('#vgt-balloon span').innerHTML = msg;
+    $('#vgt-balloon').style.visibility = "visible";
   }
   this.notif_info = function(msg){
     this.notif(msg, 'info');
