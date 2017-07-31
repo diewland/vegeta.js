2ef4c0756b8e73ded48b02d41862e51537bb4beb
Yuwat Chuesathuchon <yuwat@mfec.co.th>
2017-07-31 18:00:11 +0700

    revise all codes, compatible with bulma 0.5.0

--- README.md
+++ README.md
@@ -4,36 +4,25 @@
 
 JavaScript for Bulma.io
 
-### installation
-```npm install vegeta.js```
+## demo
+:zap: <a href='https://diewland.github.io/vegeta.js/'>LIVE</a>
 
 ## example
 ```javascript
-// new vegeta!
-var v = new Vegeta();
 
-v.bind_nav_toggle();    // bind navigation toggle
-v.bind_notif('.trunk'); // bind notification to <div class='.trunk' />
+// bind burger, notification
+Vegeta.bind_nav_burger();
+Vegeta.bind_notif('notif_here');
 
-// call notifications
-$('#btn-default').click(function(){
-  v.notif('Default message');
-});
-$('#btn-info').click(function(){
-  v.notif_info('Info message');
-});
-$('#btn-primary').click(function(){
-  v.notif_primary('Primary message');
-});
-$('#btn-success').click(function(){
-  v.notif_success('Success message');
-});
-$('#btn-warning').click(function(){
-  v.notif_warning('Warning message');
-});
-$('#btn-danger').click(function(){
-  v.notif_danger('Danger message');
-});
+// fire notifications
+Vegeta.notif('Default message');
+Vegeta.notif_default('Default message'); // same as above line
+Vegeta.notif_info('Info message');
+Vegeta.notif_primary('Primary message');
+Vegeta.notif_success('Success message');
+Vegeta.notif_warning('Warning message');
+Vegeta.notif_danger('Danger message');
 ```
-### demo
-:zap: <a href='https://diewland.github.io/vegeta.js/'>LIVE</a>
+
+## installation
+```npm install vegeta.js```
--- vegeta.js
+++ vegeta.js
@@ -1,76 +1,53 @@
-function Vegeta(){
-  
-  var $ = function(sel, parent){
-    parent = parent || document;
-    if (sel[0] == "#" && sel.indexOf(' ') == -1){// #id => by ID (faster), else query selector
-      return parent.getElementById( sel.slice(1) ) || parent.createElement("div");
-    }
-    return parent.querySelector(sel);
-  };
-  
-  function resetClasses(el){
-    var classes = ["is-default","is-danger","is-success","is-primary","is-warning","is-info"];
-    for (var i = 0; i < classes.length; i ++){
-      el.classList.remove(classes[i]);
-    }
-  }
-    
-  this.bind_nav_toggle = function(){
-    var $toggle = $('.nav-toggle');
-    var $menu = $('.nav-menu');
-    $toggle.addEventListener('click',function() {
-      $toggle.classList.toggle('is-active');
-      $menu.classList.toggle('is-active');
-    });
-    // https://stackoverflow.com/a/18504302/466693
-    $toggle.setAttribute('tabindex', 0);
-    $toggle.addEventListener('blur',function(e) {
-      // do not hide menu when click menu item
-      if((e.relatedTarget != null) && (!e.relatedTarget.classList.contains('nav-item'))){
-        $toggle.classList.remove('is-active');
-        $menu.classList.remove('is-active');
+var Vegeta = {
+
+  // bind navbar burger
+  // original script from http://bulma.io/documentation/components/navbar/
+  bind_nav_burger: function(){
+    document.addEventListener('DOMContentLoaded', function () {
+      // Get all "navbar-burger" elements
+      var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+      // Check if there are any nav burgers
+      if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+          $el.addEventListener('click', () => {
+            // Get the target from the "data-target" attribute
+            var target = $el.dataset.target;
+            var $target = document.getElementById(target);
+            // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+            $el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+          });
+        });
       }
     });
-  };
-  
-  this.bind_notif = function(sel){
-    $(sel).innerHTML = `
-      <div style='display: none;' id='vgt-balloon' class="notification">
-        <button class="delete"></button>
-        <span class="msg">Loading..</span>
-      </div>
-    `;
-    $('#vgt-balloon .delete').addEventListener('click',function(){
-      $('#vgt-balloon').style.display = "none";
+  },
+
+  // notifications
+  bind_notif: function(target_id){
+    document.getElementById(target_id).innerHTML =
+            "<div id='vgt-balloon' class='notification' style='display: none;'>"
+          + "<button id='vgt-balloon-x' class='delete'></button>"
+          + "<span id='vgt-balloon-msg'>Loading..</span>"
+          + "</div>";
+    document.getElementById('vgt-balloon-x').addEventListener('click', function(){
+      this.parentNode.style.display = 'none';
     });
-  };
-  
-  this.notif = function(msg, type){
-    $bal = $('#vgt-balloon');
+  },
+  notif: function(msg, type){
+    $bal = document.getElementById('vgt-balloon');
+    $bal.setAttribute('class', 'notification');
     if(type){
-      resetClasses($bal);
-      $bal.classList.add('is-'+type);
+      $bal.classList.add('is-'+ type);
     }
-    $bal.classList.add('notification');
-    $('.msg',$bal).innerHTML = msg;
-    $bal.style.display = "";
-  }
-  this.notif_default = function(msg){
-    this.notif(msg, 'default');
-  }
-  this.notif_info = function(msg){
-    this.notif(msg, 'info');
-  }
-  this.notif_primary = function(msg){
-    this.notif(msg, 'primary');
-  }
-  this.notif_success = function(msg){
-    this.notif(msg, 'success');
-  }
-  this.notif_warning = function(msg){
-    this.notif(msg, 'warning');
-  }
-  this.notif_danger = function(msg){
-    this.notif(msg, 'danger');
-  }
+    document.getElementById('vgt-balloon-msg').innerHTML = msg;
+    $bal.style.display = 'block';
+  },
+  notif_default: function(msg){ this.notif(msg, 'default'); },
+  notif_info:    function(msg){ this.notif(msg, 'info');    },
+  notif_primary: function(msg){ this.notif(msg, 'primary'); },
+  notif_success: function(msg){ this.notif(msg, 'success'); },
+  notif_warning: function(msg){ this.notif(msg, 'warning'); },
+  notif_danger:  function(msg){ this.notif(msg, 'danger');  },
+
 }
